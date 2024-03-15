c9a469dbe11a4ab4574f2d3dab88e2fc4d698a5f
zshdev <shohruz.zubaidov@gmail.com>
2024-03-15 06:28:34 -0400

    Lesson 24

--- ToDo/views.py
+++ ToDo/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from todos.models import Task
 
 def home(request):
-    context = {}
+    tasks_false = Task.objects.filter(is_completed=False)
+    tasks_true = Task.objects.filter(is_completed=True)
+    context = {
+        'tasks_false' : tasks_false,
+        'tasks_true' : tasks_true,
+    }
     return render(request, 'home.html', context)
