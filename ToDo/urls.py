48d81f6b63240c03eefb8a5983290bd3696f9485
zshdev <shohruz.zubaidov@gmail.com>
2024-03-15 16:40:54 -0400

    lesson 36

--- ToDo/urls.py
+++ ToDo/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    
+    #ToDo
+    path('todos/', include('todos.urls'))
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def addTask(request):
+    return HttpResponse('The form is submited')
