d8ba295ff32e0a6a862d6495ff6c7d42b321c168
zshdev <shohruz.zubaidov@gmail.com>
2024-03-15 06:40:42 -0400

    lesson 35

--- todos/admin.py
+++ todos/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Task
 
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['task', 'is_completed', 'created_at', 'updated_at']
+    search_fields = ['task',]
+
+admin.site.register(Task, TaskAdmin)
